e35d7f66f437bc882741f0333482edffc3696661
ajmal4414 <ajmalsckl@gmail.com>
2024-08-06 12:25:22 +0530

    first commit

--- hospital_management_system/hospital_management_system/settings.py
+++ hospital_management_system/hospital_management_system/settings.py
@@ -11,17 +11,19 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
+if not SECRET_KEY:
+    raise ValueError("The SECRET_KEY environment variable is not set.")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
